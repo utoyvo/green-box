5eb8ec9f2995ec88eaa252eb550b80275eb13229
Oleksandr Klochko <utoyvo@protonmail.com>
2021-01-02 15:32:13 +0200

    Initial commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1609593035,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"assets/img/garbageshadow-sheet0.png",
+		"assets/img/trash-sheet0.png",
+		"assets/img/garbage-sheet0.png",
+		"assets/img/garbage-sheet1.png",
+		"assets/img/garbage-sheet2.png",
+		"assets/img/wall-sheet0.png",
+		"assets/img/poster-sheet0.png",
+		"assets/img/okparticles.png",
+		"assets/img/noparticles.png",
+		"assets/img/gamebar-sheet0.png",
+		"assets/img/mask-sheet0.png",
+		"assets/img/dot-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
